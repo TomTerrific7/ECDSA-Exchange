b42f1a57b38ef3dd736d22b0874add2c8db99c51
Dilan Tompkins <dilantompkins@gmail.com>
2021-07-18 18:14:15 -0400

    First commit

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 import "./index.scss";
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
 const server = "http://localhost:3042";
 
@@ -19,9 +22,21 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  //insert private key
+  const privateKey = document.getElementById("privateKey").value;
+
+  const transaction = {
+    amount, recipient, privateKey, publicKey
+  }
+
+  //allows user to supply private key to front-end
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+
+  //users private key needs to sign the transaction
+  const signature = key.sign(SHA256(JSON.stringify(transaction)).toString());
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    transaction, signature: signature.toDER('hex'), publicKey: key.getPublic().encode('hex')
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
